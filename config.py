cde17edb042350822fa976eadc1088883346083c
Davidyy8 <davidolayarodriguez@gmail.com>
2026-05-30 15:49:25 +0200

    fix

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 # config.py
 VAPID_PRIVATE_KEY = "krmeMKlow9fvvzJwWvbtmDkYjNG3xFsebpvJBX7mS4M"
-VAPID_CLAIMS = {
-    "sub": "mailto:davidolayarodriguez@gmail.com" # Tu email, necesario para el protocolo
-}
+VAPID_CLAIMS = {"sub": "mailto:davidcorreoard@gmail.com"}
