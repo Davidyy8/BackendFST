6c04bd1a800f04fe12924dd8a2fdf883e216b57e
Davidyy8 <davidolayarodriguez@gmail.com>
2026-05-30 14:06:56 +0200

    notis

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-VAPID_PRIVATE_KEY = "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgGcexezKjmrpQzKlEGHn4QMSw/2mc0YCLa+AV0w7UEWyhRANCAATpTTSJlKPdKJNGQ8i1K0lR5Rs73uPGTSUfexP8wE7dCqWQfwwW8Rxi05lrkaUpGS9PXlHxiAL342elmqWMr2dW"
+VAPID_PRIVATE_KEY = "krmeMKlow9fvvzJwWvbtmDkYjNG3xFsebpvJBX7mS4M"
 VAPID_CLAIMS = {
     "sub": "davidolayarodriguez@gmail.com" # Tu email, necesario para el protocolo
 }
